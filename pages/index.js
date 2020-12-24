9aec1bbc7318e25d189d4008654f1f25f5dd3671
Gabriel Alcântara <galcantaradev@gmail.com>
2020-12-24 18:37:36 -0300

    init next app portfolio

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <Head>
+      <title>Gabriel Alcântara</title>
+
+      <h1>Hello, i'm Gabriel Alcântara</h1>
+      <p>A Software Engineer that loves frontend.</p>
+      <p>React Enthusiast.</p>
+    </Head>
+  );
+};
+
+export default Home;
