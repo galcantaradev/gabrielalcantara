0e0b2b540de81733c57b37ee8a376b6f8450bc59
Gabriel Alcântara <galcantaradev@gmail.com>
2021-01-31 16:11:08 -0300

    fix icons size

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -41,22 +41,22 @@ const NavBar = () => {
       </div>
       <div id="links">
         <a href="https://github.com/galcantaradev" target="_blank">
-          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faGithub} size="lg" />
         </a>
         <a
           href="https://www.linkedin.com/in/gabriel-alc%C3%A2ntara-4b7890120/"
           target="_blank"
         >
-          <FontAwesomeIcon icon={faLinkedinIn} />
+          <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
         </a>
         <a
           href="https://stackoverflow.com/users/14178125/gabriel-alc%c3%a2ntara"
           target="_blank"
         >
-          <FontAwesomeIcon icon={faStackOverflow} />
+          <FontAwesomeIcon icon={faStackOverflow} size="lg" />
         </a>
         <a href="https://twitter.com/galcantaradev" target="_blank">
-          <FontAwesomeIcon icon={faTwitter} />
+          <FontAwesomeIcon icon={faTwitter} size="lg" />
         </a>
       </div>
     </NavContainer>
