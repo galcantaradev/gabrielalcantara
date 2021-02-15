d07082492a662f9a9c8bc84ff285c2a6a2a06265
Gabriel Alcântara <galcantaradev@gmail.com>
2021-02-15 08:01:09 -0300

    update layout test

--- components/tests/Layout.test.tsx
+++ components/tests/Layout.test.tsx
@@ -6,6 +6,6 @@ describe('Layout.test.tsx', () => {
   it('should render correctly', () => {
     const { asFragment } = render(<Layout>Children</Layout>);
 
-    expect(asFragment).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
